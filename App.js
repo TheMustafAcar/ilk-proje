20da84451712b74776bc8f8f3abb08e27ecc611e
TheMustafAcar <ma485376@gmail.com>
2023-12-24 14:23:06 +0300

    commit

--- App.js
+++ App.js
@@ -4,17 +4,56 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.view1}>
+        <Text style={styles.text}>Mustafa ACAR 221120221083</Text>
+      </View>
+      <View style={styles.view2}>
+        <View style={styles.box}></View>
+        <View style={styles.boxs}>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+        </View>
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
+
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
+    justifyContent: 'space-around',
+    flexDirection: 'column',
+  },
+  view1: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  view2: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 22,
+  },
+  box: {
+    height: 35,
+    width: 35,
+    backgroundColor: 'blue',
+    margin: 10,
+    flexDirection: 'row',
+  },
+  boxs: {
+    flexDirection: 'row'
   },
 });
